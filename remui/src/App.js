72fc68328c046f2a332f7321095d1612a0444db5
vincentgordon97 <66184004+vincentgordon97@users.noreply.github.com>
2023-04-24 19:23:25 -0400

    Added a button

---
+++ remui/src/App.js
@@ -0,0 +1,39 @@
+import { Button, Container } from '@mui/material';
+import { ThemeProvider } from "@mui/system";
+import { useEffect } from 'react';
+import theme from "./styles/theme"
+
+function App() {
+  useEffect(() => {
+    document.title = "React Material UI - Home";
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <Container
+            maxWidth="xl"
+            sx={{
+              background: '#fff'
+            }}
+          >
+          {
+            /*
+            Appbar
+            Banner
+            Promotions
+            Title
+            Products
+            Footer
+            Searchbox
+            Appdrawer
+            */ 
+          }
+          <Button variant='contained'>Test</Button>      
+        </Container>
+    </ThemeProvider>
+
+    
+      
+  );
+}
+
+export default App;
